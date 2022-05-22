fe81144c19a745a78ad3e406fb97646f311243a4
Ivanov Vlad <lrn@2ch.hk>
2022-05-22 16:04:50 +0300

    initial commit

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -5,17 +5,50 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Map;
 
 public class TodoServer {
-    //...
+    private int port;
+    private final Todos todos;
 
     public TodoServer(int port, Todos todos) {
-        //...
+        this.port = port;
+        this.todos = todos;
     }
 
+    @SuppressWarnings("InfiniteLoopStatement")
     public void start() throws IOException {
-        System.out.println("Starting server at " + port + "...");
-        //...
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                    String json = in.readLine();
+                    Gson gson = new Gson();
+                    Map<String, String> map = gson.fromJson(json, Map.class); //это костыль?
+
+                    String action = "", task = "";
+                    for (Map.Entry<String, String> entry : map.entrySet()) {
+                        String key = entry.getKey();
+                        String value = entry.getValue();
+                        if (key.equals("type")) {
+                            action = value;
+                        } else {
+                            task = value;
+                        }
+                    }
+                    if (action.equals("ADD")) {
+                        todos.addTask(task);
+                    } else if (action.equals("REMOVE")) {
+                        todos.removeTask(task);
+                    }
+
+                    out.println(todos.getAllTasks());
+
+                }
+            }
+        }
     }
 }
--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,21 +1,25 @@
 package ru.netology.javacore;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+    private final List<String> list;
+
+    public Todos() {
+        list = new ArrayList<>();
+    }
 
     public void addTask(String task) {
-        //...
+        list.add(task);
     }
 
     public void removeTask(String task) {
-        //...
+        list.remove(task);
     }
 
     public String getAllTasks() {
-        //...
+
+        return list.toString();
     }
 
 }
