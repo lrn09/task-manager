dc41777756ddb433f9324261c01daae9bb9d9055
Ivanov Vlad <lrn@2ch.hk>
2022-06-05 16:24:18 +0300

    test class added

--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,22 @@
 package ru.netology.javacore;
 
-public class TodosTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-    // ваши тесты для класса Todos
+public class TodosTests {
+    Todos sut = new Todos();
+
+    @Test
+    public void AddToListTest(){
+        sut.addTask("Task");
+        Assertions.assertNotNull(sut.getAllTasks());
+
+    }
+    @Test
+    public void RemoveFromListTest(){
+        sut.removeTask(sut.getAllTasks());
+        Assertions.assertFalse(sut.getAllTasks().contains("Task"));
+    }
+
+
 }
